2e6fa055e23481f6de00877aedcadff3a43fb9b4
Weronika Ormaniec <weronika.ormaniec@gmail.com>
2018-11-09 19:38:25 +0100

    try

---
+++ app.vue
@@ -0,0 +1,105 @@
+<template>
+    <div id="app">
+
+        <div class="note">
+            {{ status }}
+        </div>
+
+        <div class="canvas-container">
+            <canvas id="input-canvas"
+                    width="140"
+                    height="140"
+                    @mousemove="draw"
+            />
+        </div>
+
+        <button id="clear_value" @click="clear">Clear</button>
+        <button id="inference" @click="inference">
+            Inference
+        </button>
+
+        <table>
+            <tr>
+                <th>Digits</th>
+                <th>Probability</th>
+            </tr>
+            <tr v-for="(pred, index) in preds">
+                <td>{{ index }}</td>
+                <td>{{ readable(pred) }}</td>
+            </tr>
+        </table>
+
+    </div>
+</template>
+
+<script>
+    import Predictor from './predictor';
+    const MODEL_URL = 'model/model.json';
+    //const WEIGHTS_URL = 'weights_manifest.json';
+    const HEIGHT = 28;
+    const WIDTH = 28;
+    const pixels = [];
+    for (let i = 0; i < HEIGHT * WIDTH; i++) {
+        pixels[i] = 0;
+    }
+    const preds = [];
+    for (let i = 0; i < 10; i++) {
+        preds[i] = 0;
+    }
+    export default {
+        name: 'app',
+        components: {},
+        data: function () {
+            return {
+                preds: preds,
+                predictor: new Predictor(),
+                pixels: pixels,
+                status: "Loading...",
+            }
+        },
+        created: function() {
+            this.predictor.load(MODEL_URL).then(() => {
+                this.status = "Model loaded!";
+            });
+        },
+        methods: {
+            readable(x) {
+                return x.toFixed(3);
+            },
+            draw(e) {
+                if (e.buttons == 1) {
+                    const ctx = document.getElementById('input-canvas').getContext('2d');
+                    ctx.fillStyle = "rgb(0,0,0)";
+                    ctx.fillRect(e.offsetX, e.offsetY, 8, 8);
+                    const x = Math.floor(e.offsetY * 0.2);
+                    const y = Math.floor(e.offsetX * 0.2) + 1;
+                    for (let dy = 0; dy < 2; dy++){
+                        for (let dx = 0; dx < 2; dx++){
+                            if ((x + dx < WIDTH) && (y + dy < HEIGHT)) {
+                                this.pixels[(y+dy) + (x+dx) * HEIGHT] = 1
+                            }
+                        }
+                    }
+                }
+            },
+            clear() {
+                const ctx = document.getElementById('input-canvas').getContext('2d');
+                ctx.fillStyle = "rgb(255,255,255)";
+                ctx.fillRect(0, 0, 140, 140);
+                for (var i = 0; i < 28*28; i++) {
+                    this.pixels[i] = 0;
+                }
+            },
+            inference() {
+                this.predictor.predict(this.pixels).then((value) => {
+                    this.preds = Array.prototype.slice.call(value);
+                });
+            }
+        }
+    }
+</script>
+
+
+<style scoped>
+
+</style>
